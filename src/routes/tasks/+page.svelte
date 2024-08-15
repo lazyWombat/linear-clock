f0832ab6e07bc984fab511843314a0dbd6b2af4f
lazyWombat <sukhovsky.waste@gmail.com>
2024-08-15 17:58:29 +1000

    add form (wip); add license

---
+++ src/routes/tasks/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from '../$types';
+	import TaskForm from './task-form.svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="w-full justify-center px-20 py-10">
+	<div class="m-10 mx-auto">
+		<h1 class="text-center font-clockFont text-4xl tracking-tight">Tasks</h1>
+		<TaskForm data={data.form} />
+	</div>
+</div>
---
+++ src/routes/tasks/task-form.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { formSchema, type FormSchema } from './schema';
+
+	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	export let data: SuperValidated<Infer<FormSchema>>;
+
+	const form = superForm(data, {
+		validators: zodClient(formSchema)
+	});
+
+	const { form: formData, enhance } = form;
+</script>
+
+<form method="POST" use:enhance>
+	<Form.Field {form} name="task">
+		<Form.Control let:attrs>
+			<Form.Label>Task</Form.Label>
+			<Input {...attrs} bind:value={$formData.task} />
+		</Form.Control>
+		<Form.Description>Enter a task to add it to the list.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Button>Submit</Form.Button>
+</form>
