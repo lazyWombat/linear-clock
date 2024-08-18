57c73c9cefdc259976c843c7bbb7a21cedaeb761
lazyWombat <sukhovsky.waste@gmail.com>
2024-08-18 11:31:05 +1000

    add page compatible with old browsers

---
+++ src/routes/compat/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Clock from '$lib/components/clock.svelte';
+
+	export const prerender = true;
+	export const csr = false;
+</script>
+
+<div class="w-full justify-center px-20 py-10">
+	<Clock />
+	<script>
+		setTimeout(function () {
+			location.reload();
+		}, 30000);
+	</script>
+</div>
